a9b86c5f213250c34c01ef1b6fe540f3a1e82369
ashish.sharma <ashish.wrk06@gmail.com>
2023-07-19 00:14:15 +0530

    adding rasndompractice sessions

--- day_19/listners.py
+++ day_19/listners.py
@@ -1,12 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
-for i in range(0, 4):
-    turtle1 = Turtle(shape="turtle")
-    turtle1.penup()
-    turtle1.color("red")
-    turtle1.goto(-230, -100)
+colorOfTurtle = ["red", "yellow", "green", "blue", "purple", "orange"]
+distance1 = [-100, -70, -40, -10, 20, 50]
+all_turtles = []
+
+for i in range(0, 6):
+    newTurtle = Turtle(shape="turtle")
+    newTurtle.penup()
+    newTurtle.color(colorOfTurtle[i])
+    newTurtle.goto(-230, distance1[i])
+    all_turtles.append(newTurtle)
 
 sc = Screen()
 sc.setup(width=500, height=400)
 user_bet = sc.textinput(title="Make your bet", prompt="Which turtle will win enter the color: ")
+
+is_race_on = False
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"you Won and {winning_color} color turtle wins")
+            else:
+                print(f"you Lost and {winning_color} color turtle wins")
+        rand_=random.randint(0,10)
+        turtle.forward(rand_)
+
 sc.exitonclick()
