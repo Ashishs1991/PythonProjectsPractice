b86bd4609ba1dc017479c09a646687c15e38658f
ashish.sharma <ashish.wrk06@gmail.com>
2023-07-20 23:53:12 +0530

    adding rasndompractice sessions

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -1,30 +1,27 @@
+from turtle import Screen
+from snake import Snake
 import time
-from turtle import Turtle, Screen
-
-dist = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for i in range(3):
-    new_turtle = Turtle("square")
-    new_turtle.color("white")
-    new_turtle.penup()
-    new_turtle.goto(dist[i])
-    segments.append(new_turtle)
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-screen.exitonclick()
+snake = Snake()
+# food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
-    for seg in segments:
-        seg.forward(20)
-        time.sleep(1)
+    time.sleep(0.1)
 
+    snake.move()
+
+screen.exitonclick()
